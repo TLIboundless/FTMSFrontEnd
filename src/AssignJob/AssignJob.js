698e5f9016745f8547d292393ff6a32d30ca6154
matth <matthieuchanchee@gmail.com>
2018-11-15 15:24:14 -0500

    Assign job. But it's ugly. Need to get some sort of container.

--- src/AssignJob/AssignJob.js
+++ src/AssignJob/AssignJob.js
@@ -12,54 +12,69 @@ import InputLabel from '@material-ui/core/InputLabel';
 export default class AssignJob extends Component {
   state = {
     selectedWorkOrder: '',
-    selectedEmployee: 1000,
+    selectedEmployee: '',
   }
 
+  // Need to send the data to the backend
   onSubmit = () => {
     alert(JSON.stringify(this.state))
   }
 
+  // We need info from backend for the following 2 methods
   fetchWorkOrders = () => {
-    let workOrder1 = { id: '111', location: 'Toronto', deadline: '7 Dec', skillsReq: ['Swimming'], description: 'Swim in AC' }
-    let workOrder2 = { id: '123', location: 'Ryerson', deadline: '8 Dec', skillsReq: ['Petting a dog'], description: 'Pet a dog in front of Ryerson' }
+    let workOrder1 = { id: 111, location: 'Toronto', deadline: '7 Dec', skillsReq: ['Swimming'], description: 'Swim in AC' }
+    let workOrder2 = { id: 123, location: 'Ryerson', deadline: '8 Dec', skillsReq: ['Petting a dog'], description: 'Pet a dog in front of Ryerson' }
 
     return [workOrder1, workOrder2]
   }
 
   fetchEmployees = () => {
-    let employee1 = 10
-    let employee2 = 20
+    let employee1 = { id: 10, name: 'Paul Gries' }
+    let employee2 = { id: 20, name: 'Mike McCarthy' }
 
     return [employee1, employee2]
   }
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
-    alert(event.target.name+ "   "+ event.target.value + "    " +this.state.selectedEmployee)
   };
 
   render() {
     const employeeList = this.fetchEmployees()
+    const workOrderList = this.fetchWorkOrders()
 
     return (
       <div className="App">
-      <form>
-        <FormControl>
-          <InputLabel>Choose employee</InputLabel>
-          <Select
-            input={<Input name="selectedEmployee"/>}
-            value={this.state.selectedEmployee}
-            onChange={this.handleChange}
-          >
+        <form>
+          <FormControl>
+            <InputLabel>Choose employee</InputLabel>
+            <Select
+              input={<Input name="selectedEmployee" />}
+              value={this.state.selectedEmployee}
+              onChange={this.handleChange}
+            >
+              {employeeList.map(employee => (
+                <MenuItem value={employee.name}>{employee.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <br />
+          <FormControl>
+            <InputLabel>Choose work order to assign</InputLabel>
+            <Select
+              input={<Input name="selectedWorkOrder" />}
+              value={this.state.selectedWorkOrder}
+              onChange={this.handleChange}
+            >
 
-          {employeeList.map(employee => (
-            <MenuItem value={employee}>{employee}</MenuItem>
-          ))}
-          </Select>
-
-        </FormControl>
-      </form>
-      <Button variant="contained" color="primary" onClick={this.onSubmit}>
+              {workOrderList.map(workOrder => (
+                <MenuItem value={workOrder.id}>{workOrder.id}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </form>
+        <br />
+        <Button variant="contained" color="primary" onClick={this.onSubmit}>
           Send
       </Button>
       </div>
