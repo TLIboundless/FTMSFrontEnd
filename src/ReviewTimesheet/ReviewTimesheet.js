980ceaf5352b163a6413febb6aa0e87b4dba555e
Lauren <l.mason@mail.utoronto.ca>
2018-11-17 13:26:02 -0500

    Created ReviewTimesheet class.

---
+++ src/ReviewTimesheet/ReviewTimesheet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ReviewTimesheet extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Order L</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ReviewTimesheet/>, document.getElementById('app'));
