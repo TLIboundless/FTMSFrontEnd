e3432a03ebec1d60bcd51a0d26e44f7d2beb32f9
Lauren <l.mason@mail.utoronto.ca>
2018-11-17 13:59:06 -0500

    Exported ReviewTimesheet.

--- src/ReviewTimesheet/ReviewTimesheet.js
+++ src/ReviewTimesheet/ReviewTimesheet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {DisplayTimesheet} from "./DisplayTimesheet";
 
-class ReviewTimesheet extends React.Component {
+export default class ReviewTimesheet extends React.Component {
     render () {
         return (
             <div>
-                <h1>Order L</h1>
+                <h1>Order L:</h1>
+                <h1>Review Timesheet</h1>
             </div>
         );
     }
