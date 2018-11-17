f2d94d1555db06ce9f89403039c3cc06bf58813d
Lauren <l.mason@mail.utoronto.ca>
2018-11-17 14:52:41 -0500

    Added text in ReviewTimesheet.

--- src/ReviewTimesheet/ReviewTimesheet.js
+++ src/ReviewTimesheet/ReviewTimesheet.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {DisplayTimesheet} from "./DisplayTimesheet";
+//import {DisplayTimesheet} from "./DisplayTimesheet";
+
+import '../App.css';
+import Button from '@material-ui/core/Button';
+import styleToImport from "../Utilities/Util";
+
+const chosenStyle = styleToImport.styleToImport
 
 export default class ReviewTimesheet extends React.Component {
+    /*getOrderId(){
+        //Get this timesheet's order id from database. Return as a string.
+        return "L";
+    };*/
+
     render () {
         return (
-            <div>
+            <div className="App">
                 <h1>Order L:</h1>
                 <h1>Review Timesheet</h1>
+
+
             </div>
         );
     }
 }
 
-ReactDOM.render(<ReviewTimesheet/>, document.getElementById('app'));
