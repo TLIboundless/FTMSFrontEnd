7ab6a340f192ee0735bd4eb8753a3558078e5518
Lauren <l.mason@mail.utoronto.ca>
2018-11-18 16:33:01 -0500

    Removed instances of ConfirmationDialog.

--- src/ReviewTimesheet/ReviewTimesheet.js
+++ src/ReviewTimesheet/ReviewTimesheet.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import {JobInformation} from "./JobInformation";
-import {ConfirmationDialog} from "./ConfirmationDialog";
+//I commented out all instance of ConfirmationDialog. Will implement after demo.
+//import {ConfirmationDialog} from "./ConfirmationDialog.js";
 
 import Button from '@material-ui/core/Button';
 import styleToImport from '../Utilities/Util.js'
 
-const chosenStyle = styleToImport.styleToImport
+const chosenStyle = styleToImport.styleToImport;
 
 export default class ReviewTimesheet extends Component {
     constructor(props) {
         super(props);
-        this.state = {orderList: [], accept: null};
+        this.state = {orderList: []/*, accept: null, openDialog: false*/};
         this.handleApproveClick = this.handleApproveClick.bind(this);
+        this.handleRejectClick = this.handleRejectClick.bind(this);
     }
 
-    onSubmit = () => {
-        alert(JSON.stringify(this.state))
-     }
-    handleToggle = value => () => {
-       const { orderList } = this.state;
-       const currentIndex = orderList.indexOf(value);
-       const newOrderList = [...orderList];
-       if (currentIndex !== -1) {
-         newOrderList.splice(currentIndex, 1);
-       }
+    /*dialogTitle = "";
+    dialogContent = "";*/
+    workOrderID = "";
+    workerName = "";
+    startTime = "";
+    endTime = "";
 
-       this.setState({
-         orderList: newOrderList
-       });
-     };
+    componentDidMount() {
+        fetch('/task/all')
+            .then(res => res.json())
+            .then(json => this.setState({ groups: json }));
+    }
 
-    handleApproveClick() {
-        this.setState({accept: true});
+    handleApproveClick = () => {
+        /*this.setState({accept: true, openDialog: true});
+        this.dialogTitle = 'Confirm Timesheet Approval';
+        this.dialogContent = 'Are you sure that you want to approve this timesheet?';*/
+
         //To-do: Find some way of making this alert have yes/no buttons.
         //Yes button takes you back to page of all orders, and edits database
         //to represent timesheet approval.
         //No button closes alert and keeps you on screen.
     }
 
-    handleRejectClick() {
-        alert("Are you sure that you want to reject this timesheet?");
+    handleRejectClick = () => {
+        /*this.setState({accept: false, openDialog: true});
+        this.dialogTitle = 'Confirm Timesheet Rejection';
+        this.dialogContent = 'Are you sure that you want to reject this timesheet?';*/
+
         //To-do: Find some way of making this alert have yes/no buttons.
         //Yes button takes you back to page of all orders, and edits database
         //to represent timesheet rejection.
         //No button closes alert and keeps you on screen.
-    }
-
-    handleBackClick() {
-        //Change page to order page.
     }
 
      render () {
@@ -56,11 +57,6 @@ export default class ReviewTimesheet extends Component {
             //To-do: figure out how to get work order ID, worker name, start time, and end time from back-end.
             //These values are currently hard-coded.
             <div>
-                <div style={{textAlign:"left"}}>
-                    <br/>
-                    &nbsp; &nbsp; &nbsp;
-                    <Button variant="contained">Back</Button>
-                </div>
                 <h1>Review Timesheet</h1>
                 <br/>
 
@@ -80,8 +76,7 @@ export default class ReviewTimesheet extends Component {
 
                 <Button variant="contained" size="large" color="primary" onClick={this.handleApproveClick}>APPROVE</Button>
                 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                <Button variant="contained" size="large" color="primary">REJECT</Button>
-
+                <Button variant="contained" size="large" color="primary" onClick={this.handleRejectClick}>REJECT</Button>
             </div>
         );
     }
