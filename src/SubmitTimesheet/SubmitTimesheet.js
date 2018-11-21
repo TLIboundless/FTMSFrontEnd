8e8f667354da3a2d663c08e0b5b1f5388c37881d
Luke <luke.kwong@mail.utoronto.ca>
2018-11-21 15:55:13 -0500

    Moved some files around.  Changes calls in Submit Timesheet

--- src/SubmitTimesheet/SubmitTimesheet.js
+++ src/SubmitTimesheet/SubmitTimesheet.js
@@ -8,13 +8,16 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Table from '@material-ui/core/Table';
 import styleToImport from "../Utilities/Util";
+import Button from "@material-ui/core/Button/Button";
 
 const chosenStyle = styleToImport.styleToImport;
 
 export default class SubmitTimesheet extends Component {
 
   state = {
-    job_id: '1',
+    job_id: '5',
+    work_order_id: '2',
+    worker_id: '',
     location: '',
     start: '',
     end: '',
@@ -24,30 +27,70 @@ export default class SubmitTimesheet extends Component {
   };
 
   componentDidMount() {
-    fetch('/jobs/fromID', {
+    // Get associated work order (based on id, to fetch description)
+    fetch('/work_orders/get_from_id/' + this.state.work_order_id, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: {id: this.state.job_id}
     })
       .then(res => res.json())
-      .then(json => this.setState({ description: json.description }));
+      .then(json => this.setState({
+        description: json.description,
+        location: json.location
+      }));
 
-    fetch('/task/get_task', {
+    fetch('/work_orders/get_from_id/' + this.state.job_id, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: {id: this.state.job_id}
+    })
+      .then(res => res.json())
+      .then(json => this.setState({
+        worker_id: json.worker_id
+      }));
+
+    // Get tasks
+    fetch('/task/get/' + this.state.job_id, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
     })
       .then((res) => res.json())
       .then(json => this.setState({ taskList: json}));
   }
 
-  handleChange = input => event => {
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/timesheets/add', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        worker_id: this.state.worker_id,
+        job_id: this.state.job_id,
+        start_time: this.state.start,
+        end_time: this.state.end,
+      })
+    })
+      .then(alert(JSON.stringify(this.state)))
+  };
+
+  handleChangeText = input => event => {
+    this.setState({
+      [input]: event.target.value,
+    });
+  };
+
+  handlePanelOpen = input => event => {
     this.setState({
       [input]: event.target.value,
     });
@@ -60,39 +103,46 @@ export default class SubmitTimesheet extends Component {
           <TextField
             id="datetime-job-start"
             label="Start time"
-            onChange={this.handleChange('start')}
+            value={this.state.start}
+            onChange={this.handleChangeText('start')}
             type="datetime-local"
             style={chosenStyle}
+            InputLabelProps={{
+              shrink: true,
+            }}
           />
+          <br/>
           <br/>
           <TextField
             id="datetime-job-end"
             label="End time"
-            onChange={this.handleChange('end')}
+            value={this.state.end}
+            onChange={this.handleChangeText('end')}
             type="datetime-local"
             style={chosenStyle}
+            InputLabelProps={{
+              shrink: true,
+            }}
           />
           <br/>
           <br/>
-          <ExpansionPanel id="description-panel" onChange={this.handleChange('panel1')} style={chosenStyle}>
+          <ExpansionPanel id="description-panel" onChange={this.handlePanelOpen('panel1')} style={chosenStyle}>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <text>Description</text>
             </ExpansionPanelSummary>
 
             <ExpansionPanelDetails>
-              <TextField
+              <text
                 id="standard-description"
-                label="Description"
-                value={this.state.description}
-                onChange={this.handleChange('description')}
-                margin="normal"
-                multiline={true}
+                onChange={this.handleChangeText('description')}
                 style={chosenStyle}
-              />
+              >
+                {this.state.description}
+              </text>
             </ExpansionPanelDetails>
           </ExpansionPanel>
 
-          <ExpansionPanel id="task-panel" onChange={this.handleChange('panel2')} style={chosenStyle}>
+          <ExpansionPanel id="task-panel" onChange={this.handlePanelOpen('panel2')} style={chosenStyle}>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               <text>Tasks</text>
             </ExpansionPanelSummary>
@@ -100,11 +150,16 @@ export default class SubmitTimesheet extends Component {
             <ExpansionPanelDetails>
               <Table>
 
+
               </Table>
             </ExpansionPanelDetails>
           </ExpansionPanel>
 
         </form>
+        <br/>
+        <Button variant="contained" color="secondary" onClick={this.onSubmit}>
+          Send
+        </Button>
       </div>
     );
   }
